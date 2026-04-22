5a74667f9cc34f8db9a057bc30b502961fe36a1d
Aeriles <aeriles@proton.me>
2026-04-22 14:29:46 +0200

    feature: SDL3 instance creation

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,22 @@
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_vulkan.h>
+#include <vulkan/vulkan.h>
+
 int main() {
+  SDL_Init(SDL_INIT_VIDEO);
+  SDL_Window* window = SDL_CreateWindow("chemish", 1280, 720, SDL_WINDOW_VULKAN);
+
+  bool running = true;
+  while(running) {
+    SDL_Event event;
+    while(SDL_PollEvent(&event)) {
+      if(event.type == SDL_EVENT_QUIT) running = false;
+      if(event.type == SDL_EVENT_KEY_DOWN && event.key.key == SDLK_ESCAPE) running = false;
+    }
+  }
+
+  SDL_DestroyWindow(window);
+  SDL_Quit();
+
   return 0;
 }
