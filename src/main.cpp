cd8b319f3a9e888a1d23ef372dff8c42e50ccb2d
Aeriles <aeriles@proton.me>
2026-04-28 15:31:37 +0200

    following khronos vulkan tutorial

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,35 @@
-int main() { return 0; }
+#include <vulkan/vulkan.hpp>
+
+#include <SDL3/SDL.h>
+
+#include <cstdlib>
+#include <iostream>
+#include <stdexcept>
+
+class HelloTriangleApplication {
+public:
+  void run() {
+    initVulkan();
+    mainLoop();
+    cleanup();
+  }
+
+private:
+  void initVulkan() {}
+
+  void mainLoop() {}
+
+  void cleanup() {}
+};
+
+int main() {
+  try {
+    HelloTriangleApplication app;
+    app.run();
+  } catch (const std::exception &e) {
+    std::cerr << e.what() << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  return EXIT_SUCCESS;
+}
